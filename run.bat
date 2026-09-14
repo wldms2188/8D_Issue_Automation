19ca296eff2f4452fe2952ab76b3065cbda5f083
wldms2188 <wldms2188@naver.com>
2026-09-14 21:37:31 +0900

    Run recovery STEP3

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-echo [8D Issue Automation v3.2.0 RECOVERY STEP2]
-py -3 app\main_recovery_step2.py
-if errorlevel 1 py app\main_recovery_step2.py
+echo [8D Issue Automation v3.2.0 RECOVERY STEP3]
+py -3 app\main_recovery_step3.py
+if errorlevel 1 py app\main_recovery_step3.py
 if errorlevel 1 (
   echo.
   echo Program ended with an error.
