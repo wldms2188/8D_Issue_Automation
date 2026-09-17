b1465c9ad720d4a2c58c1921c7d328ac4d410a02
wldms2188 <wldms2188@naver.com>
2026-09-17 19:33:50 +0900

    Enable final attachment deduplication patch

--- app/main_enterprise_final.py
+++ app/main_enterprise_final.py
@@ -3,6 +3,7 @@ import excel_change_style_final  # noqa: F401
 from main_enterprise_v3 import EnterpriseAppV3
 import team_autocomplete_final  # noqa: F401
 import problem_summary_final  # noqa: F401
+import attachment_dedupe_final  # noqa: F401
 
 if __name__ == '__main__':
     EnterpriseAppV3().mainloop()
