a018590fddd29d11c3ec6a50954b1a57e2355a0a
wldms2188 <wldms2188@naver.com>
2026-09-17 17:36:56 +0900

    Enable standardized team autocomplete

--- app/main_enterprise_final.py
+++ app/main_enterprise_final.py
@@ -1,6 +1,7 @@
 import weekly_style_final  # noqa: F401
 import excel_change_style_final  # noqa: F401
 from main_enterprise_v3 import EnterpriseAppV3
+import team_autocomplete_final  # noqa: F401
 
 if __name__ == '__main__':
     EnterpriseAppV3().mainloop()
