6822dad35fa72a2de1f9239b0364c2100166a1d1
wldms2188 <wldms2188@naver.com>
2026-09-17 23:00:05 +0900

    Enable final output polish layer

--- app/main_enterprise_final.py
+++ app/main_enterprise_final.py
@@ -4,6 +4,7 @@ from main_enterprise_v3 import EnterpriseAppV3
 import team_autocomplete_final  # noqa: F401
 import problem_summary_final  # noqa: F401
 import attachment_dedupe_final  # noqa: F401
+import final_output_polish  # noqa: F401
 
 if __name__ == '__main__':
     EnterpriseAppV3().mainloop()
