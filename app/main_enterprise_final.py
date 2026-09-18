96e2aebcbf1aa185fd7f1556fc1c8c0f1dbfadc5
wldms2188 <wldms2188@naver.com>
2026-09-18 14:57:50 +0900

    Enable customer project autocomplete in final launcher

--- app/main_enterprise_final.py
+++ app/main_enterprise_final.py
@@ -2,6 +2,7 @@ import weekly_style_final  # noqa: F401
 import excel_change_style_final  # noqa: F401
 from main_enterprise_v3 import EnterpriseAppV3
 import team_autocomplete_final  # noqa: F401
+import project_autocomplete_final  # noqa: F401
 import problem_summary_final  # noqa: F401
 import attachment_dedupe_final  # noqa: F401
 import final_output_polish  # noqa: F401
