34b1f0b535acf232c7263a2d35596bccd572addb
wldms2188 <wldms2188@naver.com>
2026-09-18 12:38:56 +0900

    Enable final output type selection

--- app/main_enterprise_final.py
+++ app/main_enterprise_final.py
@@ -6,6 +6,7 @@ import problem_summary_final  # noqa: F401
 import attachment_dedupe_final  # noqa: F401
 import final_output_polish  # noqa: F401
 import origin_classifier_patch  # noqa: F401
+import output_variant_final  # noqa: F401
 
 if __name__ == '__main__':
     EnterpriseAppV3().mainloop()
