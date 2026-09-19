b6384a743ac1203fbec33889943dbed549f0dd89
wldms2188 <wldms2188@naver.com>
2026-09-19 17:08:37 +0900

    Prevent duplicated customer prefix in weekly project names

--- app/main_recovery_step13.py
+++ app/main_recovery_step13.py
@@ -31,6 +31,15 @@ def _customer_task(d):
     customer=N(d.get('customer'))
     task=N(d.get('task_name'))
     if customer and task:
+        # task_name can already be stored as "customer_project" when the user
+        # selects a canonical project. Collapse one or more repeated prefixes.
+        if C(task)==C(customer):
+            return customer
+        pat=re.compile(r'^(?:'+re.escape(customer)+r'\s*[_\-/／|:：]\s*)+',re.I)
+        m=pat.match(task)
+        if m:
+            rest=N(task[m.end():]).strip(' _-/／|:：')
+            return customer+('_'+rest if rest else '')
         return customer+'_'+task
     return customer or task
 
